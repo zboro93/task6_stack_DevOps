7f7029dd187c0b54b8bd9e241bcb5049e0302838
zboro93 <34000498+zboro93@users.noreply.github.com>
2026-03-02 15:34:36 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,7 +132,7 @@ resource "aws_key_pair" "deployer" {
 
 resource "aws_instance" "app_server" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   subnet_id                   = aws_subnet.public_subnet_1.id
   vpc_security_group_ids      = [aws_security_group.sg_allow_required_ports.id]
   associate_public_ip_address = true
