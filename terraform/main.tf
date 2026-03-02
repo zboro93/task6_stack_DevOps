f10d2225fd4fa03a081966d1aacf5cdcb873930b
zboro93 <34000498+zboro93@users.noreply.github.com>
2026-03-02 11:39:31 +0100

    Simplify S3 backend configuration
    Removed specific S3 backend configuration for Terraform.

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,13 +13,7 @@ terraform {
       version = "~> 4.1"
     }
   }
-  backend "s3" {
-    bucket         = "zboro93-tfstate-bucket"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "zboro93-terraform-locks"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
