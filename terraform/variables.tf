1021f186c3b69f80b09fa7b311b03193b6677790
zboro93 <34000498+zboro93@users.noreply.github.com>
2026-03-02 13:50:52 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone"
+  type        = string
+}
 
